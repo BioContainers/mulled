5354ef2089c3c05de330856b7dcf242e20a9ce5c
Jonas Weber <github@jonasw.de>
2016-01-15 22:13:17 +0100

    Import

---
+++ builder.lua
@@ -0,0 +1,281 @@
+
+-- Settings
+curl = 'appropriate/curl'
+jq = 'local_tools/jq'
+
+quay_prefix = 'mulled'
+namespace = 'quay.io/' .. quay_prefix
+github_repo = 'mulled/api'
+
+builders = {}
+
+current_build_id = 'snapshot'
+
+------------------------------------------------------------------------
+------------- ALPINE
+------------------------------------------------------------------------
+builders.alpine = function (package, revision, test, builddir)
+  repo = namespace .. '/' .. package .. ':' .. revision
+
+extractInfo = 'apk info --root /data/dist -wd  ' .. package .. [==[
+| (
+  read fline ;
+  echo $fline | cut -d" " -f1 | cut -d"-" -f 2-3 > /data/info/version;
+  read desc; echo  $desc > /data/info/description ;
+  read discard ;
+  read discard ;
+  read homepage ; echo $homepage > /data/info/homepage ;
+)
+]==]
+
+  install = [==[ apk --root /data/dist --update-cache --repository http://dl-4.alpinelinux.org/alpine/v3.2/main --keys-dir /etc/apk/keys --initdb add ]==]
+
+  inv.task('build:' .. package)
+    .using('alpine')
+      .withConfig({entrypoint = {"/bin/sh", "-c"}})
+      .withHostConfig({
+        binds = {builddir .. ':/data'}
+      })
+      .run('mkdir -p /data/dist /data/info')
+      .run(install .. package .. ' && ' .. extractInfo)
+      .run('rm -rf /data/dist/lib/apk /data/dist/var/cache/apk/')
+
+      .wrap(builddir .. '/dist').at('/').inImage('busybox:latest')
+        .as(repo)
+
+  inv.task('clean:' .. package)
+    .using('alpine')
+      .withHostConfig({binds = {builddir .. ':/data'}})
+      .run('rm', '-rf', '/data/dist', '/data/info')
+
+  inv.task('test:' .. package)
+    .using(repo)
+    .withConfig({entrypoint = {'/bin/sh', '-c'}})
+    .run(test)
+end
+
+------------------------------------------------------------------------
+------------- LINUXBREW
+------------------------------------------------------------------------
+builders.linuxbrew = function (package, revision, test, builddir)
+  repo = namespace .. '/' .. package .. ':' .. revision
+
+  extractInfo = '$BREW info --json=v1 ' .. package ..
+[==[ | jq --raw-output '.[0] | [.homepage, .desc, .versions.stable] | join("\n")' | (
+  read homepage ; echo $homepage > /info/homepage ;
+  read desc ; echo $desc > /info/description ;
+  read version ; echo $version > /info/version ;
+) ]==]
+
+  inv.task('build:' .. package)
+    .using('thriqon/linuxbrew-alpine')
+      .withConfig({user = "root"})
+      .withHostConfig({
+        binds = {builddir .. ':/data'}
+      })
+      .run('mkdir', '-p', '/data/info', '/data/dist')
+      .run('chown', 'nobody', '/data/info', '/data/dist/')
+
+      .withConfig({user = "nobody"})
+      .run('mkdir', '/data/dist/bin', '/data/dist/Cellar')
+      .withConfig({
+        user = "nobody",
+        entrypoint = {"/bin/sh", "-c"},
+        env = {
+          "BREW=/brew/orig_bin/brew",
+          "HOME=/tmp"
+        }
+      })
+      .withHostConfig({binds = {
+        builddir .. "/dist/bin:/brew/bin",
+        builddir .. "/dist/Cellar:/brew/Cellar",
+        builddir .. "/info:/info"
+      }})
+      .run('$BREW install ' .. package)
+      .run('$BREW test ' .. package)
+      .run(extractInfo)
+    .wrap(builddir .. '/dist').inImage('mwcampbell/muslbase-runtime')
+      .at("/brew/").as(repo)
+
+  inv.task('clean:' .. package)
+  .using('thriqon/linuxbrew-alpine')
+    .withConfig({user = "root"})
+    .withHostConfig({
+      binds = {builddir .. ':/data'}
+    })
+    .run('rm', '-rf', '/data/dist', '/data/info')
+
+  inv.task('test:' .. package)
+    .using(repo)
+    .withConfig({entrypoint = {'/bin/sh', '-c'}})
+    .run(test)
+end
+
+------------------------------------------------------------------------
+------------- BIOCONDA
+------------------------------------------------------------------------
+builders.bioconda = function (package, revision, test, builddir)
+  repo = namespace .. '/' .. package .. ':' .. revision
+
+  install = [==[ conda install -p /data/dist --copy --yes ]==]
+  extractInfo = 'conda list -p /data/dist -e ' .. package .. ' | grep ^' .. package .. '= | tr = - | xargs -I %% cp /anaconda/pkgs/%%/info/recipe.json /data/info/raw.json'
+
+  transformInfo = '/jq-linux64 --raw-output  \'[.about.home, .about.summary, .package.version] | join("\n")\' /data/info/raw.json | '
+    .. [==[ ( read homepage ; echo $homepage > /data/info/homepage ; read desc ; echo $desc > /data/info/description ; read version ; echo $version > /data/info/version ) ]==]
+
+  linkLib64 = 'ln -s /lib /data/dist/lib64'
+
+  inv.task('build:' .. package)
+	.using('bioconda/bioconda-builder')
+		.withConfig({entrypoint = {"/bin/sh", "-c"}})
+		.withHostConfig({
+			binds = {builddir .. ':/data'}
+		})
+		.run('mkdir -p /data/dist /data/info')
+   .run(install .. package .. ' && ' .. extractInfo)
+   .run(linkLib64)
+
+	.using(jq)
+		.withHostConfig({binds = {builddir .. ':/data'}})
+		.run(transformInfo)
+	.wrap(builddir .. '/dist').at('/').inImage('busybox:ubuntu')
+    .as(repo)
+
+  inv.task('clean:' .. package)
+    .using('bioconda/bioconda-builder')
+      .withConfig({entrypoint = {"/bin/sh", "-c"}})
+      .withHostConfig({
+        binds = {builddir .. ':/data'}
+      })
+      .run('rm -rf /data/dist /data/info')
+
+  inv.task('test:' .. package)
+    .using(repo)
+    .withConfig({entrypoint = {'/bin/sh', '-c'}})
+    .run(test)
+end
+
+function pushTask(package, new_revision, packager, builddir)
+  inv.task('push:' .. package)
+  -- tag as latest
+    .tag(namespace .. '/' .. package .. ':' .. new_revision)
+      .as(namespace .. '/' .. package)
+  -- create repo if needed
+    .using(curl)
+      .withConfig({env = {"TOKEN=" .. ENV.TOKEN}})
+      .run('/bin/sh',  '-c', 'curl --fail -X POST -HAuthorization:Bearer\\ $TOKEN -HContent-Type:application/json '
+          .. '-d \'{"namespace": "' .. quay_prefix .. '", "visibility": "public", '
+          .. '"repository": "' .. package .. '", "description": ""}\' '
+          .. 'https://quay.io/api/v1/repository || true')
+  -- upload image
+    .using('docker')
+      .withHostConfig({
+        binds = {
+          "/var/run/docker.sock:/var/run/docker.sock",
+          ENV.HOME .. "/.docker:/root/.docker",
+          builddir .. ':/pkg'
+        }
+      })
+      .run('/bin/sh', '-c', 
+        'docker push ' .. namespace .. '/' .. package .. ':' .. new_revision .. ' | grep digest | grep "' .. new_revision .. ': " | cut -d" " -f 3 > /pkg/info/checksum')
+      .run('docker', 'push', namespace .. '/' .. package)
+      .run('/bin/sh', '-c',
+        'docker inspect -f "{{.VirtualSize}}" ' .. namespace .. '/' .. package .. ':' .. new_revision .. ' > /pkg/info/size')
+  -- generate new description
+    .using(jq)
+      .withHostConfig({binds = {
+        builddir .. ':/pkg',
+        './data:/data'
+      }})
+      .run('('
+        .. 'echo "# ' .. package .. '" ; echo; '
+        .. 'echo -n "> "; cat /pkg/info/description; echo; '
+        .. 'cat /pkg/info/homepage; echo; '
+        .. 'echo "Latest revision: ' .. new_revision .. '"; echo; '
+        .. 'echo "---" ; echo; '
+        .. 'echo "## Available revisions"; echo; '
+        .. '/jq-linux64 --raw-output \'(.' .. package .. '//[]) | map("* " + .) | join("\n")\' /data/quay_versions; '
+        .. 'echo "* ' .. new_revision .. '" ; echo; '
+        .. 'echo ) | /jq-linux64 --raw-input --slurp \'{description: .}\' > /pkg/info/quay_description')
+
+  -- put new description
+    .using(curl)
+      .withHostConfig({binds = {builddir .. ':/pkg'}})
+      .withConfig({env = {"TOKEN=" .. ENV.TOKEN}})
+      .run('/bin/sh', '-c', 'curl --fail -HAuthorization:Bearer\\ $TOKEN -T /pkg/info/quay_description  '
+          .. '-HContent-type:application/json '
+          .. 'https://quay.io/api/v1/repository/' .. quay_prefix .. '/' .. package)
+  -- fetch current SHA
+    .using(jq)
+      .withHostConfig({binds = {builddir .. ':/pkg', './data:/data'}})
+      .run('/jq-linux64 --raw-output \'map(select(.name == "' .. package .. '.json"))[0].sha\' /data/github_repo > /pkg/info/previous_file_sha')
+  -- generate image json
+    .using(jq)
+      .withHostConfig({binds = {builddir .. ':/pkg'}})
+      .run('/jq-linux64 --raw-input --slurp \'.|split("\n") as $i | {'
+        .. 'message: "build ' .. ENV.TRAVIS_BUILD_NUMBER .. '\n\nbuild url: ' .. current_build_id .. '", '
+        .. 'content: ("---\n---\n" + ({'
+          .. 'image: "' .. package .. '",'
+          .. 'date: (now | todate),'
+          .. 'buildurl: "' .. current_build_id .. '",'
+          .. 'packager: "' .. packager .. '", '
+          .. 'homepage: $i[0], description: $i[1], version: $i[2], checksum: $i[3], size: $i[4]'
+        .. '} | tostring) | @base64), '
+        -- todo add checksum size
+        .. 'sha: $i[5]}\' /pkg/info/homepage /pkg/info/description /pkg/info/version /pkg/info/checksum /pkg/info/size /pkg/info/previous_file_sha  > /pkg/info/github_commit')
+  -- put image json to github
+    .using(curl)
+      .withHostConfig({binds = {builddir .. ':/pkg'}})
+      .withConfig({env = {"TOKEN=" .. ENV.GITHUB_TOKEN}})
+      .run('/bin/sh',  '-c', 'curl --fail -HAuthorization:Bearer\\ $TOKEN -HContent-Type:application/json '
+        .. '-T /pkg/info/github_commit https://api.github.com/repos/' .. github_repo .. '/contents/_images/' .. package .. '.json')
+end
+
+-- split line by tabs
+function split(s)
+  local start = 1
+  local t = {}
+  while true do
+    local pos = string.find(s, "\t", start, true)
+    if not pos then
+      break
+    end
+    table.insert(t, string.sub(s, start, pos - 1))
+    start = pos + 1
+  end
+  table.insert(t, string.sub(s, start))
+  return t
+end
+
+-- registered packages
+registered_packages = {}
+for line in io.lines("packages.tsv") do
+  fields = split(line)
+  registered_packages[fields[2]] = fields
+end
+
+pr = inv.task('pr')
+prod = inv.task('prod')
+
+local build_counter = 0
+
+-- build tasks
+for line in io.lines("data/build_list") do
+  local fields = registered_packages[line]
+
+  local packager = fields[1]
+  local package = fields[2]
+  local revision = fields[3]
+  local test = fields[4]
+
+  assert(builders[packager])
+  build_counter = build_counter + 1
+  builddir = './mulled-build-' .. tostring(build_counter)
+  builders[packager](package, revision, test, builddir)
+
+  pushTask(package, revision, packager, builddir)
+
+  pr.runTask('build:' .. package).runTask('test:' .. package).runTask('clean:' .. package)
+  prod.runTask('build:' .. package).runTask('test:' .. package).runTask('push:' .. package).runTask('clean:' .. package)
+end
+
---
+++ generate_lists.lua
@@ -0,0 +1,84 @@
+
+-- Settings
+
+curl = 'appropriate/curl'
+jq = 'local_tools/jq'
+
+namespace = 'mulled'
+github_repo = 'mulled/api'
+
+-- Tasks
+
+inv.task('main:generate_jq_image')
+  .using('busybox')
+    .run('mkdir', '-p', 'jq')
+  .using('appropriate/curl')
+    .run('--location', 'https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64', '-o', 'jq/jq-linux64')
+  .using('busybox')
+    .run('chmod', 'a+x', 'jq/jq-linux64')
+  .wrap('jq').at('/')
+    .withConfig({entrypoint = {'/bin/sh', '-c'}})
+    .inImage('busybox').as(jq)
+  .using('busybox')
+    .run('rm', '-rf', 'jq')
+
+
+parseDescriptions = '[.repositories[] |' -- use the repositories field
+  .. '{key: .name, value: ' -- the values are generated next
+  .. ' .description | split("---")[1] |' -- split the description into header and footer, separated by ---
+  .. 'split("\n") |' -- split into lines
+  .. 'map(select(startswith("* "))[2:])' -- take all lines starting with "* " and remove that prefix
+  .. '}] | from_entries' -- output object
+
+inv.task('main:load_versions_from_quay')
+  .using(curl).run('https://quay.io/api/v1/repository?public=true&namespace=' .. namespace, '-o', 'data/quay_repository_search')
+  .using(jq).run('/jq-linux64 \'' .. parseDescriptions .. '\' data/quay_repository_search > data/quay_versions')
+
+
+parsePackages = 'split("\n") |' -- split input string into lines
+  .. 'map(select(startswith("#") == false)) |' -- drop comments
+  .. 'map(select((. | length) > 0)) |' -- drop empty lines (e.g. last line)
+  .. 'map(split("\t")) |' -- split into tab delimited fields
+  .. 'map({key: .[1], value: .[2]}) | from_entries' -- create object with names as keys, version as value
+
+inv.task('main:load_versions_from_packages.tsv')
+  .using(jq).run('/jq-linux64 --slurp --raw-input \'' .. parsePackages .. '\' packages.tsv > data/local_versions')
+
+
+computeBuildList = '. as [$remotes, $locals] |' -- use variables for convenience
+  .. '$locals | to_entries |' -- convert to entries array and map over them
+  .. 'map(.key as $k | .value as $v | select('
+  .. '  false == (' -- select(false == is comparable to reject
+  .. '    ($remotes[$k]//[]) |' -- fetch the versions array (remotely) for that package, default to empty
+  .. '    contains([$v]) ))) |' -- and answer whether this exact version is already present
+  .. ' map(.key) | join("\n")' -- extract the package name (the key) and join by newlines
+inv.task('main:generate_list:builds')
+  .using(jq).run('/jq-linux64 --slurp --raw-output \'' .. computeBuildList .. '\' data/quay_versions data/local_versions > data/build_list')
+
+computeDeleteList = '. as [$remotes, $locals] |' -- use variables for convenience
+  .. '$remotes | keys |' -- extract remotely available package names
+  .. 'map(. as $k | select( ($locals | has($k)) == false))' -- select all keys that are not present in the local package list
+  .. '| join("\n")' -- join with newlines
+inv.task('main:generate_list:deletes')
+  .using(jq).run('/jq-linux64 --raw-output --slurp \'' .. computeDeleteList .. '\' data/quay_versions data/local_versions > data/delete_list')
+
+inv.task('main:generate_lists')
+  .runTask('main:generate_list:builds')
+  .runTask('main:generate_list:deletes')
+
+inv.task('main:create_data_dir')
+  .using('busybox')
+    .run('mkdir', '-p', 'data')
+
+inv.task('main:fetch_images_dir_from_github')
+  .using(curl)
+    .run('https://api.github.com/repos/' .. github_repo .. '/contents/_images', '-o', 'data/github_repo')
+
+inv.task('main:prepare')
+  .runTask('main:create_data_dir')
+  .runTask('main:generate_jq_image')
+  .runTask('main:load_versions_from_quay')
+  .runTask('main:load_versions_from_packages.tsv')
+  .runTask('main:generate_lists')
+  .runTask('main:fetch_images_dir_from_github')
+
